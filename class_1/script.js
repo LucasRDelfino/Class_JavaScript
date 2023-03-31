87e631f207dd993ce6ef9091536b31bab0bbda62
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-03-31 20:51:41 -0300

    sorting out the exercise in functions by js

--- class_1/script.js
+++ class_1/script.js
@@ -1,2 +1,26 @@
+function alerta() {
 alert ("Hello World")
 console.log("TESTANDO")
+}
+
+function nome(){
+const nome = prompt("Qual o seu nome")
+alert("Olá " + nome)
+}
+
+function dobro(){
+const num = prompt("Número:" )
+const dobro = num * 2 
+alert("O dobro é " + dobro)
+}
+
+function soma() {
+   const num1 = Number(prompt("1º Número :    "))
+   const num2 = Number(prompt("2 Número :    "))
+   const soma = num1 + num2
+   alert(`A soma é ${soma}`)
+}
+
+
+
+dobro()
