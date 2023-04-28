0c50ff1746df196d2133ef7cfafd8880ac829ab3
Lucas Rodrigues Delfino <89103771+LucasRDelfino@users.noreply.github.com>
2023-04-28 20:58:04 -0300

    adding arithmetic operations class

---
+++ class_3/operacoes.js
@@ -0,0 +1,21 @@
+var x = 275;
+var y = 96;
+var z = x + y;
+document.getElementById("adicao").innerHTML='A soma de '+ x +' com '+ y +' é igual a '+ z;
+
+var x = 275;
+var y = 96;
+var z = x - y;
+document.getElementById("subtracao").innerHTML='A subtração de '+ x +' com '+ y +' é igual a '+ z;
+
+var x = 275;
+var y = 96;
+var z = x * y;
+document.getElementById("multiplicacao").innerHTML='A multiplicação de '+ x +' com '+ y +' é igual a '+ z;
+
+var x = 275;
+var y = 96;
+var z = x / y;
+document.getElementById("divisao").innerHTML='A divisão de '+ x +' com '+ y +' é igual a '+ z.toFixed(2);
+
+
